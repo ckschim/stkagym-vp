331be5835f06d86136737430f3d625a5901f9a44
Lars Funke <larsfunke1996@gmail.com>
2012-10-28 23:49:39 +0100

    Added subgrade support (automatic disabling is not working), fixed theme, other shtuff.

--- src/com/fqsmobile/stkagym_vp/SettingsActivity.java
+++ src/com/fqsmobile/stkagym_vp/SettingsActivity.java
@@ -1,14 +1,27 @@
 package com.fqsmobile.stkagym_vp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends PreferenceActivity{
+public class SettingsActivity extends PreferenceActivity {
 	 
     @SuppressWarnings("deprecation")
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-             }
+	protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	addPreferencesFromResource(R.xml.preferences);
+    }
+    
+    public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key){
+    	//TODO: Funktioniert nicht. Keine Ahnung warum.
+    	if(key.equals("grades_list")) {
+    		String grade = sharedPreferences.getString("grades_list", "");
+    		if(grade.equals("EF") || grade.equals("Q1") || grade.equals("Q2"))
+    			getPreferenceScreen().findPreference("subgrades_list").setEnabled(false);
+    		else
+    			getPreferenceScreen().findPreference("subgrades_list").setEnabled(true);
+    	}
+    		
+    };
 }
