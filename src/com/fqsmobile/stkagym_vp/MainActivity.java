5c4ca1bd04aaefe90ca13b9143d4cd4775547838
Cedrik Schimschar <cskamen23@gmail.com>
2012-10-29 21:20:52 +0100

    added refresh button, which restarts the mainactivity. the main activity now reads the shared_preferences from startup. live changes visible after restart or refresh

--- src/com/fqsmobile/stkagym_vp/MainActivity.java
+++ src/com/fqsmobile/stkagym_vp/MainActivity.java
@@ -1,17 +1,42 @@
 package com.fqsmobile.stkagym_vp;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.SharedPreferences;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends Activity {
+ 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String grade = getPrefs(); 
+        TextView gradeTextView = (TextView)findViewById(R.id.grade);  //Zum Start lädt er dann direkt den String von den Einstellungen
+        gradeTextView.setText(grade);
+        
+        final Button button = (Button) findViewById(R.id.button_refresh);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Intent intent = getIntent();
+            	finish();
+            	startActivity(intent);
+            }
+            });
+        }
+    
+    //Hier holt der sich einfach nur den gewählten String der Stufenauswahl. Selbsterkärend, denke ich.
+    String ListPref;
+    public String getPrefs()
+    {
+    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+    	ListPref = prefs.getString("grades_list", "");
+    	return ListPref;
+    	
     }
 
     @Override
