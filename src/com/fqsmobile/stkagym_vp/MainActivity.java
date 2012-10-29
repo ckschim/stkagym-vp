edea0b113ef16a0d15bab6f131c2f979f5744506
Cedrik Schimschar <cskamen23@gmail.com>
2012-10-29 22:49:31 +0100

    fixed refresh button (better solution), formatting fixed and changed way of loading preferences

--- src/com/fqsmobile/stkagym_vp/MainActivity.java
+++ src/com/fqsmobile/stkagym_vp/MainActivity.java
@@ -9,72 +9,82 @@ import android.view.*;
 import android.widget.*;
 
 public class MainActivity extends Activity {
- 
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        String grade = getGradePrefs(); 
-        TextView gradeTextView = (TextView)findViewById(R.id.grade);  //Zum Start lädt er dann direkt den String von den Einstellungen
-        gradeTextView.setText(grade);
-        String subgrade = getSubgradePrefs();
-        TextView subgradeTextView = (TextView)findViewById(R.id.subgrade);
-        if(grade.equals("Q1") || grade.equals("Eph") || grade.equals("Q2")) //Vermeidung von unerwünschten Zusätzen zur Oberstufe durch alte Einträge :D
-        {
-        	subgradeTextView.setText("");
-        }
-        else
-        {
-        subgradeTextView.setText(subgrade);
-        }
-        
-        final Button button = (Button) findViewById(R.id.button_refresh);  //Refresh-Button
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	Intent intent = getIntent();
-            	finish();
-            	startActivity(intent);
-            }
-            });
-        }
-    
-    //Hier holt der sich einfach nur den gewählten String der Stufenauswahl. Selbsterkärend, denke ich.
-    String ListGradePref;
-    public String getGradePrefs()
-    {
-    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-    	ListGradePref = prefs.getString("grades_list", "");
-    	return ListGradePref;
-    	
-    }
-    String ListSubgradePref;
-    public String getSubgradePrefs()
-    {
-    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-    	ListSubgradePref = prefs.getString("subgrades_list", "");
-    	return ListSubgradePref;
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.menu_settings:
-            	Intent activity_intent = new Intent();
-            	activity_intent.setClass(getApplicationContext(), SettingsActivity.class);
-            	startActivity(activity_intent); 
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-    
-   
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		/* Auslesen der Einstellungen beim Start */
+		String grade = getGradePrefs();
+		TextView gradeTextView = (TextView) findViewById(R.id.grade);
+		gradeTextView.setText(grade);
+		String subgrade = getSubgradePrefs();
+		TextView subgradeTextView = (TextView) findViewById(R.id.subgrade);
+		subgradeTextView.setText(subgrade);
+
+		final Button button = (Button) findViewById(R.id.button_refresh); // Refresh-Button
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				/*
+				 * Hier wird werden wiederdie Einstellungen ausgelesen
+				 */
+				String grade = getGradePrefs();
+				TextView gradeTextView = (TextView) findViewById(R.id.grade);
+				gradeTextView.setText(grade);
+				String subgrade = getSubgradePrefs();
+				TextView subgradeTextView = (TextView) findViewById(R.id.subgrade);
+				subgradeTextView.setText(subgrade);
+			}
+		});
+	}
+
+	/*
+	 * Hier holt der sich einfach nur den gewählten String der Stufenauswahl.
+	 * Selbsterkärend, denke ich.
+	 */
+
+	public String getGradePrefs() {
+		String ListGradePref;
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getBaseContext());
+		ListGradePref = prefs.getString("grades_list", "");
+		return ListGradePref;
+
+	}
+
+	public String getSubgradePrefs() {
+		String ListSubgradePref;
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getBaseContext());
+		ListSubgradePref = prefs.getString("subgrades_list", "");
+		return ListSubgradePref;
+	}
+
+	/* Erstellung des Menu-Button Menüs */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	/*
+	 * Verhalten bei Auswahl eines Items des Menüs
+	 * 
+	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			Intent activity_intent = new Intent();
+			activity_intent.setClass(getApplicationContext(),
+					SettingsActivity.class);
+			startActivity(activity_intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
 }
--- src/com/fqsmobile/stkagym_vp/SettingsActivity.java
+++ src/com/fqsmobile/stkagym_vp/SettingsActivity.java
@@ -6,24 +6,26 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
-	 
-    @SuppressWarnings("deprecation")
+
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-    	addPreferencesFromResource(R.xml.preferences);
-    
-	    final ListPreference gradesList = (ListPreference)findPreference("grades_list");
-	    final ListPreference subgradesList = (ListPreference)findPreference("subgrades_list");
-	    
-	    gradesList.setOnPreferenceChangeListener(
-		    new Preference.OnPreferenceChangeListener() {
-		    	public boolean onPreferenceChange(Preference preference, Object newValue){
-		        	final String val = newValue.toString();
-		        	subgradesList.setEnabled(!(val.equals("EF") || val.equals("Q1") || val.equals("Q2")));
-		        	return true;
-		    	}
-		    }
-		);
-    }
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+
+		final ListPreference gradesList = (ListPreference) findPreference("grades_list");
+		final ListPreference subgradesList = (ListPreference) findPreference("subgrades_list");
+
+		gradesList
+				.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						final String val = newValue.toString();
+						subgradesList.setEnabled(!(val.equals("EF")
+								|| val.equals("Q1") || val.equals("Q2")));
+						subgradesList.setValue(""); // Leeren
+						return true;
+					}
+				});
+	}
 }
