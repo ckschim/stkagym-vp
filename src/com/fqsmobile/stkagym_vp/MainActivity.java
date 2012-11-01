e33e920ac8079147298be2ea4637a846aba3945d
Cedrik Schimschar <cskamen23@gmail.com>
2012-11-01 13:35:42 +0100

    main activity closes after switching to settings. textview is scrollable now (not tested though)

--- src/com/fqsmobile/stkagym_vp/MainActivity.java
+++ src/com/fqsmobile/stkagym_vp/MainActivity.java
@@ -18,6 +18,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.text.method.ScrollingMovementMethod;
 import android.view.*;
 import android.widget.*;
 
@@ -35,7 +36,6 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		/* Anwenden der Einstellungen beim Start */
 		applySettings();
-
 		final Button button = (Button) findViewById(R.id.button_refresh); // Refresh-Button
 		button.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
@@ -72,6 +72,7 @@ public class MainActivity extends Activity {
 
 		TextView substTextView = (TextView) findViewById(R.id.substitution_data);
 		substTextView.setText(res);
+		substTextView.setMovementMethod(new ScrollingMovementMethod());
 	}
 
 	public void applySettings() {
@@ -121,6 +122,7 @@ public class MainActivity extends Activity {
 			Intent settings_intent = new Intent();
 			settings_intent.setClass(getApplicationContext(), SettingsActivity.class);
 			startActivity(settings_intent);
+			this.finish();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
